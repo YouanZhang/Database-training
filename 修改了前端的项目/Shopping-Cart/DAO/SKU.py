56bdcdb54b117965ae125fdb60e8cf89ea1ea7e6
SalTang2099 <48648389+SalTang2099@users.noreply.github.com>
2020-06-23 01:49:40 +0800

    Upload SKU, SPU

---
+++ 修改了前端的项目/Shopping-Cart/DAO/SKU.py
@@ -0,0 +1,16 @@
+from link_database import *
+
+#添加SKU
+def addSPU(name, desc, price, SpuID, qty, city):
+    conn = link_mysql()
+    cur = conn.cursor()
+    cur.execute('INSERT INTO SKU VALUES (NULL, %s, %s, %s, %s, %s, %s);',(name, desc, price, SpuID, qty, city))
+    conn.commit()
+    cur.close()
+    conn.close()
+    print('INSERT INTO SKU VALUES (NULL, %s, %s, %s, %s);',(name, desc, price, SpuID))
+
+#addSPU('华为P40Pro 8GB+128GB 冰霜银','华为P40Pro，5G协议，存储量256GB, 银色', 5988, 3)
+addSPU('华为P40Pro 8GB+128GB 红色','华为P40Pro，5G协议，存储量128GB, 红色', 5988, 3, 100, '广州')
+#addSPU('华为P40Pro 8GB+256GB 零度白','华为P40Pro，5G协议，存储量256GB, 白色', 6488, 3)
+#addSPU('华为P40Pro 8GB+128GB 零度白','华为P40Pro，5G协议，存储量128GB, 白色', 5988, 3)
