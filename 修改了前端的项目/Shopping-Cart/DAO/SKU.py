b4970a582a8e600ce0a3f5dad0129101ae73f352
yangjianscut <43467348+yangjianscut@users.noreply.github.com>
2020-06-23 11:29:14 +0800

    modify sku spu

--- 修改了前端的项目/Shopping-Cart/DAO/SKU.py
+++ 修改了前端的项目/Shopping-Cart/DAO/SKU.py
@@ -1,16 +1,39 @@
-from link_database import *
+from DAO.link_database import *
 
 #添加SKU
-def addSPU(name, desc, price, SpuID, qty, city):
+def addSKU(name, desc, price, SpuID, qty, city):
     conn = link_mysql()
     cur = conn.cursor()
-    cur.execute('INSERT INTO SKU VALUES (NULL, %s, %s, %s, %s, %s, %s);',(name, desc, price, SpuID, qty, city))
+    cur.execute('INSERT INTO SKU VALUES (NULL, %s, %s, %s, %s, %s, %s);', (name, desc, price, SpuID, qty, city))
     conn.commit()
     cur.close()
     conn.close()
-    print('INSERT INTO SKU VALUES (NULL, %s, %s, %s, %s);',(name, desc, price, SpuID))
+    print('INSERT INTO SKU VALUES (NULL, %s, %s, %s, %s);', (name, desc, price, SpuID))
 
-#addSPU('华为P40Pro 8GB+128GB 冰霜银','华为P40Pro，5G协议，存储量256GB, 银色', 5988, 3)
-addSPU('华为P40Pro 8GB+128GB 红色','华为P40Pro，5G协议，存储量128GB, 红色', 5988, 3, 100, '广州')
-#addSPU('华为P40Pro 8GB+256GB 零度白','华为P40Pro，5G协议，存储量256GB, 白色', 6488, 3)
-#addSPU('华为P40Pro 8GB+128GB 零度白','华为P40Pro，5G协议，存储量128GB, 白色', 5988, 3)
+#查找SKU
+def findSKUbyid(id):
+    conn = link_mysql()
+    cur = conn.cursor()
+    cur.execute('SELECT * FROM SKU WHERE `sku_id` = %s', (id,))
+    data = cur.fetchall()
+    # print('SELECT * FROM BUYER WHERE `e_mail` = %s AND `password` = %s', (email, password))
+    vaild = len(data) != 0
+    cur.close()
+    conn.close()
+    return vaild, data
+
+def findSKUbyname(name):
+    conn = link_mysql()
+    cur = conn.cursor()
+    cur.execute('SELECT * FROM SKU WHERE `sku_name` = %s', (name, ))
+    data = cur.fetchall()
+    # print('SELECT * FROM BUYER WHERE `e_mail` = %s AND `password` = %s', (email, password))
+    vaild = len(data) != 0
+    cur.close()
+    conn.close()
+    return vaild, data
+#addSKU('华为P40Pro 8GB+128GB 冰霜银','华为P40Pro，5G协议，存储量256GB, 银色', 5988, 3)
+#addSKU('华为P40Pro 8GB+128GB 红色','华为P40Pro，5G协议，存储量128GB, 红色', 5988, 3, 100, '广州')
+#addSKU('华为P40Pro 8GB+256GB 零度白','华为P40Pro，5G协议，存储量256GB, 白色', 6488, 3)
+#addSKU('华为P40Pro 8GB+128GB 零度白','华为P40Pro，5G协议，存储量128GB, 白色', 5988, 3)
+#print(findSKUbyid(3))
--- 修改了前端的项目/Shopping-Cart/DAO/SPU.py
+++ 修改了前端的项目/Shopping-Cart/DAO/SPU.py
@@ -1,4 +1,4 @@
-from link_database import *
+from DAO.link_database import *
 
 #添加SPU
 def addSPU(name, desc):
@@ -10,5 +10,27 @@ def addSPU(name, desc):
     conn.close()
     print('INSERT INTO SPU (SPU_ID,SPU_NAME,SPU_DESC) VALUES (NULL, %s, %s);',(name, desc))
 
+def findSPUbyid(id):
+    conn = link_mysql()
+    cur = conn.cursor()
+    cur.execute('SELECT * FROM SPU WHERE `spu_id` = %s', (id,))
+    data = cur.fetchall()
+    # print('SELECT * FROM BUYER WHERE `e_mail` = %s AND `password` = %s', (email, password))
+    vaild = len(data) != 0
+    cur.close()
+    conn.close()
+    return vaild, data
+
+def findSPUbyname(name):
+    conn = link_mysql()
+    cur = conn.cursor()
+    cur.execute('SELECT * FROM SPU WHERE `spu_name` = %s', (name, ))
+    data = cur.fetchall()
+    # print('SELECT * FROM BUYER WHERE `e_mail` = %s AND `password` = %s', (email, password))
+    vaild = len(data) != 0
+    cur.close()
+    conn.close()
+    return vaild, data
 #addSPU('Huawei P40 Pro','华为P40Pro手机，全网通，4G/5G')
 #addSPU('Huawei P40','华为P40手机，全网通，4G/5G')
+#print(findSPUbyid(2))
