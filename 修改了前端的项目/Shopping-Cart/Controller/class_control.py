6fbd1e21932155c04bbe2120b7f35988709e4d3e
yangjianscut <43467348+yangjianscut@users.noreply.github.com>
2020-06-24 13:02:34 +0800

    Update class_control.py

--- 修改了前端的项目/Shopping-Cart/Controller/class_control.py
+++ 修改了前端的项目/Shopping-Cart/Controller/class_control.py
@@ -5,7 +5,8 @@ def getLeftList():
     data = getNameIdFromSecondClass()
     for i in range(len(data)):
         spulist = findSPUbySecondClassId(data[i][0])
-        data[i] = [data[i]] + spulist
+        data[i] = [data[i]] + [spulist]
+        #print(i, ' ',len(data[i][1]) )
     return data
-
 #print(getLeftList())
+#print(len(getLeftList()[1][1]))
