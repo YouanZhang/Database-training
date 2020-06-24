c88ca6c4d986a1776562e08ca79c802a6258af40
yangjianscut <43467348+yangjianscut@users.noreply.github.com>
2020-06-24 10:55:58 +0800

    modify secnd_class

---
+++ 修改了前端的项目/Shopping-Cart/DAO/order.py
@@ -0,0 +1,3 @@
+from DAO.link_database import *
+
+def addNewOrder():
--- 修改了前端的项目/Shopping-Cart/DAO/second_class.py
+++ 修改了前端的项目/Shopping-Cart/DAO/second_class.py
@@ -1,12 +1,13 @@
 from DAO.link_database import *
-
+#还没写
 def getNameIdFromSecondClass():
     conn = link_mysql()
     cur = conn.cursor()
-    cur.execute('SELECT * FROM  WHERE `sku_id` = %s', (id,))
+    cur.execute('SELECT `SECOND_CLASS_ID`, `CLASS_NAME` FROM SECOND_CLASS')
     data = cur.fetchall()
     # print('SELECT * FROM BUYER WHERE `e_mail` = %s AND `password` = %s', (email, password))
-    vaild = len(data) != 0
     cur.close()
     conn.close()
-    return vaild, data
+    return data
+
+#print(getNameIdFromSecondClass())
