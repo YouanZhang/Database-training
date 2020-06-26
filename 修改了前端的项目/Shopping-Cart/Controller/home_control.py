b598b570a80bf5d3c6bca1c3921b47d9eedc4a66
yangjianscut <43467348+yangjianscut@users.noreply.github.com>
2020-06-26 12:09:06 +0800

    Update home_control.py

--- 修改了前端的项目/Shopping-Cart/Controller/home_control.py
+++ 修改了前端的项目/Shopping-Cart/Controller/home_control.py
@@ -7,4 +7,12 @@ def getRecommPage(buyer_id):
             list += data
     return list
 
+def getFirstPageForNotLogin():
+    list = []
+    for i in range(4):
+        valid, data = findSKUbyid(i)
+        if valid:
+            list += data
+    return list
+
 #print(getRecommPage(list))
