461e2dc0c9430a93481646112e936e4a126c22dc
yangjianscut <43467348+yangjianscut@users.noreply.github.com>
2020-06-27 14:48:25 +0800

    modify mer

--- 修改了前端的项目/Shopping-Cart/DAO/link_redis.py
+++ 修改了前端的项目/Shopping-Cart/DAO/link_redis.py
@@ -1,6 +1,15 @@
 import redis
 
-r = redis.Redis(host='127.0.0.1',port=6379,db=0)
-r.set('hello','world')
-print(r.keys())
-print(r.get('hello'))
+def link_redis():
+    pool = redis.ConnectionPool(host='localhost', port=6379, decode_responses=True)
+    r = redis.Redis(connection_pool=pool)
+    return r
+
+#r = link_redis()
+#data = r.lrange(233, 0, -1)
+#int_data = [int(x)for x in data]
+#r.lpush(234, data)
+#print(int_data)
+#r.set('hello','world')
+#print(r.keys())
+#print(r.get('hello'))
--- 修改了前端的项目/Shopping-Cart/DAO/mer_collection.py
+++ 修改了前端的项目/Shopping-Cart/DAO/mer_collection.py
@@ -1,9 +1,12 @@
 from DAO.link_database import *
+from DAO.link_redis import *
 
 def addMerColletion(buyer_id, sku_id):
     conn = link_mysql()
     cur = conn.cursor()
     cur.execute('INSERT INTO MER_COLLECTION VALUES (%s,%s);', (buyer_id, sku_id))
+    r = link_redis()
+    r.rpush(buyer_id, sku_id)
     conn.commit()
     cur.close()
     conn.close()
@@ -11,11 +14,14 @@ def addMerColletion(buyer_id, sku_id):
 def findMerCollectionByBuyerID(buyer_id):
     conn = link_mysql()
     cur = conn.cursor()
+    r = link_redis()
+    rdata = r.lrange(buyer_id, 0, -1)
+    int_data = [int(x)for x in rdata]
     cur.execute('SELECT * FROM MER_COLLECTION WHERE `BUYER_ID` = %s',(buyer_id,))
     data = cur.fetchall()
     cur.close()
     conn.close()
-    return data
+    return int_data
 
 def dropMerCollection(buyer_id, sku_id):
     conn = link_mysql()
