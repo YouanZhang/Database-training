3893260d384eb21c6c295b578f988651fe2fd5a3
yangjianscut <43467348+yangjianscut@users.noreply.github.com>
2020-06-27 20:12:30 +0800

    modify

--- 修改了前端的项目/Shopping-Cart/DAO/mer_collection.py
+++ 修改了前端的项目/Shopping-Cart/DAO/mer_collection.py
@@ -1,6 +1,7 @@
 from DAO.link_database import *
 from DAO.link_redis import *
 from DAO.SKU import *
+redislink = link_redis()
 def addMerColletion(buyer_id, sku_id):
     conn = link_mysql()
     cur = conn.cursor()
@@ -9,8 +10,7 @@ def addMerColletion(buyer_id, sku_id):
         if x == sku_id:
             return
     cur.execute('INSERT INTO MER_COLLECTION VALUES (%s,%s);', (buyer_id, sku_id))
-    r = link_redis()
-    r.rpush(buyer_id, sku_id)
+    redislink.rpush(buyer_id, sku_id)
     conn.commit()
     cur.close()
     conn.close()
@@ -18,8 +18,7 @@ def addMerColletion(buyer_id, sku_id):
 def findMerCollectionByBuyerID(buyer_id):
     #conn = link_mysql()
     #cur = conn.cursor()
-    r = link_redis()
-    rdata = r.lrange(buyer_id, 0, -1)
+    rdata = redislink.lrange(buyer_id, 0, -1)
     int_data = [int(x)for x in rdata]
     retlist = []
     for i in range(len(int_data)):
@@ -33,8 +32,7 @@ def findMerCollectionByBuyerID(buyer_id):
     return retlist
 
 def dropMerCollection(buyer_id, sku_id):
-    r = link_redis()
-    r.lrem(buyer_id, 0, sku_id)
+    redislink.lrem(buyer_id, 0, sku_id)
     conn = link_mysql()
     cur = conn.cursor()
     cur.execute('DELETE FROM MER_COLLECTION WHERE buyer_id = %s AND sku_id = %s',(buyer_id, sku_id))
